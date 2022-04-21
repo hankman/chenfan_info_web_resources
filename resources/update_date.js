2b144d90d1a9da3fdb90fd6cee0813473673092e
Chen Fan <c-fan@outlook.com>
2022-04-21 14:21:40 +0800

    add logic to prevent useless query

--- resources/update_date.js
+++ resources/update_date.js
@@ -1,5 +1,29 @@
+function parse_curr_date()
+{
+    let update_date_a = document.getElementsByClassName("date-link")[0];
+    date_str = update_date_a.textContent
+    if (!date_str) {
+        return null;
+    }
+    y = parseInt(date_str);
+    m = parseInt(date_str.slice(date_str.indexOf("年")+1));
+    d = parseInt(date_str.slice(date_str.indexOf("月")+1));
+    return new Date(y, m-1, d);
+}
+
+
 function refresh_update_date()
 {
+    up_date = parse_curr_date();
+    if (up_date) {
+        today = new Date();
+        if (today.getYear() == up_date.getYear() &&
+            today.getMonth() == up_date.getMonth() &&
+            today.getDate() == up_date.getDate() + 1) {
+                return;
+            }
+    }
+
     const xhttp_date = new XMLHttpRequest();
     xhttp_date.onload = function() {
         let update_date_a = document.getElementsByClassName("date-link")[0];
@@ -12,5 +36,5 @@ function refresh_update_date()
 
 window.onload = function() {
     refresh_update_date();
-    window.setInterval(refresh_update_date, 5*60*1000);
+    window.setInterval(refresh_update_date, 10*60*1000);
 }
