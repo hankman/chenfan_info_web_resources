52898bf7cf78804ac2b95e382b896a7251cb885f
Chen Fan <c-fan@outlook.com>
2022-05-29 14:52:41 +0800

    Add domain switch link to pages

--- resources/long_search.js
+++ resources/long_search.js
@@ -3,7 +3,18 @@ let url_parts= decodeURI(window.location.pathname).split("/")
 let query_str = url_parts[url_parts.length - 1]
 
 title = document.getElementById("common-title")
+domain_switch = document.getElementById("common-domain-switch")
+
 title.textContent = '"' + query_str + '"的查询结果'
+
+if (url_parts[1] == "bj")
+{
+    domain_switch.href = "/" + url_parts.slice(2).join("/");
+}
+else
+{
+    domain_switch.href = "/bj" + window.location.pathname;
+}
 
 const xhttp_long_search_data = new XMLHttpRequest();
 xhttp_long_search_data.onload = function() {
